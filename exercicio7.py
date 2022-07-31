d16ce147719e2781b0246e9b18c5bc08d4350e9e
GugaFarias <102561230+GugaFarias@users.noreply.github.com>
2022-07-31 19:48:56 -0300

    Create exercicio7.py

---
+++ exercicio7.py
@@ -0,0 +1,12 @@
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+
+altura = float(input("Digite o valor da altura do quadrado: "))
+base = altura
+
+area_quadrado = altura * base
+
+aq = 2 * area_quadrado
+
+print(area_quadrado)
+print(aq)
